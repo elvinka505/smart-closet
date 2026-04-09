f1a4952e76ad2a6426a46983370403ccde625adb
elvinka505 <gazetdiinovaelvina@gmail.com>
2026-04-09 19:30:42 +0300

    минимальные изменения

--- src/main/java/ru/isgaij/smartcloset/entity/User.java
+++ src/main/java/ru/isgaij/smartcloset/entity/User.java
@@ -1,7 +1,6 @@
 package ru.isgaij.smartcloset.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
